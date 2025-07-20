979dbdd8c3d0f244ab2014a702312277d2278323
jello2025 <albarjas2025@gmail.com>
2025-07-20 23:49:26 +0300

    finally done. took lotsa patience

--- src/transactions.ts
+++ src/transactions.ts
@@ -20,9 +20,7 @@ const transactions: Transaction[] = [
 // example:
 // filterIncomeTransactions(transactions); // => [["income", 1000], ["income", 1500], ["income", 700]]
 function filterIncomeTransactions(transactions: Transaction[]): Transaction[] {
-  // write your code here...
-
-  return []; // replace empty array with what you see is fit
+  return transactions.filter((transaction) => transaction.includes("income"));
 }
 
 // `filterExpenseTransactions` function that:
@@ -31,9 +29,7 @@ function filterIncomeTransactions(transactions: Transaction[]): Transaction[] {
 // example:
 // filterExpenseTransactions(transactions); // => [["expense", 500], ["expense", 300]]
 function filterExpenseTransactions(transactions: Transaction[]): Transaction[] {
-  // write your code here...
-
-  return []; // replace empty array with what you see is fit
+  return transactions.filter((transaction) => transaction.includes("expense"));
 }
 
 // `calculateTotalIncome` function that:
@@ -41,33 +37,68 @@ function filterExpenseTransactions(transactions: Transaction[]): Transaction[] {
 // - calculates the total income and returns it.
 // example:
 // calculateTotalIncome(transactions); // => 3200  (1000 + 1500 + 700)
-function calculateTotalIncome(transactions: Transaction[]): number {
-  // write your code here...
+function calculateTotalIncome(transactions: Transaction[]): any {
+  let incomeOnly = transactions.filter((transaction) =>
+    transaction.includes("income")
+  );
 
-  return -1; // replace -1 with what you see is fit
+  let result = incomeOnly.reduce((acc, curr) => {
+    return acc + curr[1];
+  }, 0);
+
+  console.log(result);
+  return result;
 }
+
+// calculateTotalIncome(transactions);
 
 // `calculateTotalExpenses` function that:
 // - Accepts a "transactions" parameter of type "Transaction[]".
 // - calculates the total expense and returns it.
 // example:
 // calculateTotalExpenses(transactions); // => 800  (500 + 300)
-function calculateTotalExpenses(transactions: Transaction[]): number {
-  // write your code here...
+function calculateTotalExpenses(transactions: Transaction[]): any {
+  let expenseOnly = transactions.filter((transaction) =>
+    transaction.includes("expense")
+  );
 
-  return -1; // replace -1 with what you see is fit
+  let result = expenseOnly.reduce((acc, curr) => {
+    return acc + curr[1];
+  }, 0);
+
+  console.log(result);
+  return result;
 }
+// calculateTotalExpenses(transactions);
 
 // `calculateNetTotal` function that:
 // - Accepts a "transactions" parameter of type "Transaction[]".
 // - calculates the net total (total income - total expenses) and returns it.
 // example:
 // calculateNetTotal(transactions); // => 2400  (3200 - 800)
-function calculateNetTotal(transactions: Transaction[]): number {
-  // write your code here...
+function calculateNetTotal(transactions: Transaction[]) {
+  let incomeOnly = transactions.filter((transaction) =>
+    transaction.includes("income")
+  );
 
-  return -1; // replace -1 with what you see is fit
+  let result1 = incomeOnly.reduce((acc, curr) => {
+    return acc + curr[1];
+  }, 0);
+
+  let expenseOnly = transactions.filter((transaction) =>
+    transaction.includes("expense")
+  );
+
+  let result2 = expenseOnly.reduce((acc, curr) => {
+    return acc + curr[1];
+  }, 0);
+
+  let total = result1 - result2;
+  console.log(total);
+  return total;
 }
+
+// calculateNetTotal(transactions);
 
 // `filterSignificantTransactions` function that:
 // - Accepts 2 parameters:
@@ -80,11 +111,11 @@ function filterSignificantTransactions(
   transactions: Transaction[],
   threshold: number
 ): Transaction[] {
-  // write your code here...
-
-  return []; // replace empty array with what you see is fit
+  return transactions.filter((item) => {
+    return item[1] >= threshold;
+  });
 }
-
+filterSignificantTransactions(transactions, 500);
 export {
   Transaction,
   filterIncomeTransactions,
